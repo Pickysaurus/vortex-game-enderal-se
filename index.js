7c72a3ef254791526b4ed60c7f0ab5e6d33890a9
Pickysaurus <pickysaurus@googlemail.com>
2021-03-08 21:20:39 +0000

    Added launcher icon.
    Added cleaner check for missing requirement.

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-const { util } = require('vortex-api');
+const { fs, log, util } = require('vortex-api');
 const path = require('path');
 
 const GAME_ID = 'enderalspecialedition';
 const STEAMAPP_ID = '976620';
 
 // Files needed for Enderal to run correctly.
-const requiredFiles = [
-    'SkyrimSE.exe',
-    'skse64_loader.exe',
-    'Enderal Launcher.exe',
-    path.join('Data', 'Enderal - Forgotten Stories.esm'),
-    path.join('Data', 'Skyrim.esm'),
-    path.join('Data', 'SkyUI_SE.esp'),
-    path.join('Data', 'SKSE', 'Plugins', 'JContainers64.dll'),
-    path.join('Data', 'SKSE', 'Plugins', 'FlatMapMarkersSSE.dll'),
-];
+// const requiredFiles = [
+//     'SkyrimSE.exe',
+//     'skse64_loader.exe',
+//     'Enderal Launcher.exe',
+//     path.join('Data', 'Enderal - Forgotten Stories.esm'),
+//     path.join('Data', 'Skyrim.esm'),
+//     path.join('Data', 'SkyUI_SE.esp'),
+//     path.join('Data', 'SKSE', 'Plugins', 'JContainers64.dll'),
+//     path.join('Data', 'SKSE', 'Plugins', 'FlatMapMarkersSSE.dll'),
+// ];
+
+const fileChecks = {
+    'skse64_loader.exe': {
+        relPath: 'skse64_loader.exe',
+        url: 'https://skse.silverlock.org/',
+        name: 'Skyrim Script Extender 64 (Current SE Build 2.0.19+)'
+    },
+    'Enderal - Forgotten Stories.esm': {
+        relPath: path.join('Data', 'Enderal - Forgotten Stories.esm'),
+        url: 'https://www.nexusmods.com/enderalspecialedition/mods/1',
+        name: 'Enderal Special Edition'
+    },
+    'SkyUI_SE.esp': {
+        relPath: path.join('Data', 'SkyUI_SE.esp'),
+        url: 'https://www.nexusmods.com/skyrimspecialedition/mods/12604',
+        name: 'SkyUI Special Edition'
+    },
+    'JContainers64.dll': {
+        relPath: path.join('Data', 'SKSE', 'Plugins', 'JContainers64.dll'),
+        url: 'https://www.nexusmods.com/skyrimspecialedition/mods/16495',
+        name: 'JContainers64'
+    },
+    'FlatMapMarkersSSE.dll': {
+        relPath: path.join('Data', 'SKSE', 'Plugins', 'FlatMapMarkersSSE.dll'),
+        url: 'https://www.nexusmods.com/skyrimspecialedition/mods/22122',
+        name: 'FlatMapMarkers'
+    }
+}
 
 const tools = [
     {
@@ -37,64 +65,66 @@ const tools = [
         requiredFiles: [
             'Enderal Launcher.exe' 
         ],
+        logo: 'launcher-icon.jpg',
         relative: true,
         exclusive: true
     }
 ];
-
-const gamebyroData = (api) => {
-    const state = api.store.getState();
-    const gamePath = util.getSafe(state, ['settings', 'gameMode', 'discovered', 'enderalspecialedition', 'path'], undefined);
-    if (!gamePath) return undefined;
-    const skyrimInstall = gamePath.toLowerCase().includes('skyrim');
-    
-    // gamebyro-plugin-management data
-    const pluginData = {
-        appDataPath: skyrimInstall ? 'Skyrim Special Edition' : 'Enderal Special Edition',
-        pluginTXTFormat: 'fallout4',
-        nativePlugins: [
-            'skyrim.esm',
-            'update.esm',
-            'dawnguard.esm',
-            'hearthfires.esm',
-            'dragonborn.esm',
-        ]
-    }
-
-    // gamebyro-savegame-management data
-    const saveData = {
-        mygamesPath: skyrimInstall ? 'Skyrim Special Edition' : 'Enderal Special Edition',
-        iniName: skyrimInstall ? 'Skyrim.ini' : 'Enderal.ini',
-        prefIniName: skyrimInstall ? 'SkyrimPrefs.ini' : 'EnderalPrefs.ini',
-        saveFiles: (input) => {
-            const coSaveExt = 'skse';
-            return input.reduce((cur, prev) => {
-                const ext = path.extname(cur);
-                const coSave = `${path.basename(cur, ext)}.${coSaveExt}`;
-                prev.push([save, coSave]);
-                return prev;
-            }, []);
-        }
-    }
-    return { pluginData, saveData };
-}
 
 function findGame() {
     return util.GameStoreHelper.findByAppId([STEAMAPP_ID])
         .then(game => game.gamePath);
 }
 
-function prepareForModding(discovery, api) {
-    // console.log(gamebyroData(api));
-    const state = api.store.getState();
+async function prepareForModding(discovery, api) {
 
     const enderalPath = discovery.path;
-    const ssePath = util.getSafe(state, ['settings', 'gameMode', 'discovered', 'skyrimse', 'path'], undefined);
 
-    if (ssePath && pathCompare(enderalPath, ssePath)) {
-        return Promise.reject('Vortex can only manage Enderal Special Edition when installed via Steam. To use the non-Steam version, please create a profile for Skyrim Special Edition');
+    const missingDependencies = [];
+
+    const fileKeys = Object.keys(fileChecks);
+
+    for (const key of fileKeys) {
+        const check = fileChecks[key];
+        const checkPath = path.join(enderalPath, check.relPath);
+        try {
+            await fs.statAsync(checkPath);
+        }
+        catch (err) {
+            log('warn', 'Missing required file for Enderal SE', checkPath)
+            missingDependencies.push(check);
+        }
+    };
+
+    if (missingDependencies.length) {
+        api.sendNotification({
+            id: 'enderal-missing-mods',
+            type: 'warning',
+            title: 'Dependencies Missing',
+            message: 'Enderal SE is not installed correctly.',
+            actions: [
+                {
+                    title: 'More',
+                    action: (dismiss) => missingModsModal(api, missingDependencies, dismiss)
+                }
+            ]
+        })
     }
-    else return Promise.resolve(); 
+    
+    return Promise.resolve(); 
+}
+
+function missingModsModal(api, missingDependencies, dismiss) {
+    api.showDialog('warn', 'Enderal Dependencies Missing', {
+        bbcode: 'Enderal Special Edition requires several mods to be installed before it will work correctly. Vortex detected the following mods as missing: <br /><br />'+
+        missingDependencies.map(mod => `- [url=${mod.url}]${mod.name}[/url]<br />`).join('<br/>')+
+        '<br /><br />You will not be able to start playing Enderal until these mods have been installed.'
+    },[
+        {
+            label: 'Close',
+            action: () => dismiss(),
+        }
+    ])
 }
 
 function main(context) {
@@ -112,7 +142,9 @@ function main(context) {
         queryModPath: () => 'data',
         logo: 'gameart.jpg',
         executable: () => 'SkyrimSE.exe',
-        requiredFiles: requiredFiles,
+        requiredFiles: [
+            "SkyrimSE.exe"
+        ],
         compatible: {
             symlinks: false
         },
@@ -123,15 +155,6 @@ function main(context) {
 
     return true;
 
-}
-
-function pathCompare(enderal, sse) {
-    // Check if the Enderal and SSE paths are the same, if they are we want to reject the game setup.
-    const e = enderal.toLowerCase();
-    const s = sse.toLowerCase();
-    if (e == s) return true;
-    else if (e.includes(s) || s.includes(e)) return true;
-    else return false;
 }
 
 module.exports = {
